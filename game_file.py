172c87dd06c97ae04433fa8f21b70493c8241a84
Evans <evansc@andrew.cmu.edu>
2020-08-15 17:54:15 -0400

    Created the board

--- game_file.py
+++ game_file.py
@@ -1,13 +1,7 @@
-
-import pygame
-
+import pygame, sys
 from pygame.locals import *
 
 pygame.init()
-
-#set up the window
-screen = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('drawing')
 
 black = (  0,   0,   0)
 white = (255, 255, 255)
@@ -15,7 +9,49 @@ red   = (255,   0,   0)
 green = (  0, 255,   0)
 blue  = (  0,   0, 255)
 
-screen.fill(white)
+width = 900
+height = 1000
 
-pygame.draw.polygon(screen, green, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
+cellx = width/9
+celly = height/10
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Sudoku')
+
+running = True
+while running:
+
+    screen.fill(white)
+    
+    for col in range(10):
+        top = [cellx*col,0]
+        bottom = [cellx*col,height-celly]
+        if (col%3==0):
+            thickness = 3
+        else:
+            thickness = 1
+        if col == 9:
+            top[0] -= 1
+            bottom[0] -= 1
+        pygame.draw.lines(screen, black, True, [top, bottom], thickness)
+
+    for row in range(10):
+        left = [0, celly*row]
+        right = [width, celly*row]
+        if (row%3==0):
+            thickness = 3
+        else:
+            thickness = 1
+        pygame.draw.lines(screen, black, True, [left, right], thickness)
+
+
+
+    pygame.display.update()
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
