e847605d503b9798e1d7f4a06152273fbc113281
Evans <evansc@andrew.cmu.edu>
2020-08-15 14:45:47 -0400

    Added my version of solver

---
+++ solver_v1.py
@@ -0,0 +1,101 @@
+import copy
+
+class Positions:
+    def __init__(self):   
+        self.row = set()
+        self.col = set()
+        self.grid = set()
+    def add(self, row, col):
+        self.row.add(row)
+        self.col.add(col)
+        self.grid.add((row//3)*3+(col//3))
+    def delete(self, row, col):
+        self.row.remove(row)
+        self.col.remove(col)
+        self.grid.remove((row//3)*3+(col//3))
+        
+                    
+class Solution:
+    def solveSudoku(self, board): 
+        self.positions = [None]*10
+        for i in range(1,10):
+            self.positions[i] = Positions()
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if (board[row][col] != "."):
+                    self.positions[int(board[row][col])].add(row, col) 
+        board = self.solveWithBacktracking(board)
+        
+        
+    def solveWithBacktracking(self,board):
+        if self.isValidSudoku(board) and self.isComplete(board):
+            return board
+        nextStep = self.getNextStep(board)
+        for move in ["1","2","3","4","5","6","7","8","9"]:
+            # Sometimes it's easier to make a move, then check if it's valid.
+            # Sometimes it's easier to check if a move is valid first.
+            # Just make sure that you always undo a move properly!
+            if self.isValid(board, nextStep, int(move)):
+                board = self.makeMove(board, nextStep, move)
+                tmpSolution = self.solveWithBacktracking(board)
+                if tmpSolution != None:
+                    return tmpSolution
+                board = self.undoMove(board, nextStep, move)
+        return None
+        
+        
+    def isComplete(self, board):
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if (board[row][col] == "."):
+                    return False
+        return True
+    
+    def getNextStep(self, board):
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if (board[row][col] == "."):
+                    return ((row,col))
+    
+    def isValid(self, board, nextStep, move):
+        if nextStep[0] in self.positions[move].row: return False
+        if nextStep[1] in self.positions[move].col: return False
+        if (nextStep[0]//3)*3+(nextStep[1]//3) in self.positions[move].grid: return False
+        return True
+    
+    def makeMove(self, board, nextStep, move):
+        board[nextStep[0]][nextStep[1]] = move
+        self.positions[int(move)].add(nextStep[0],nextStep[1])
+        return board
+    
+    def undoMove(self, board, nextStep, move):
+        board[nextStep[0]][nextStep[1]] = "."
+        self.positions[int(move)].delete(nextStep[0],nextStep[1])
+        return board
+    
+    
+    def isValidSudoku(self, board):
+        #create 3 hash tables that maps the row/col/sub-grid to sets 
+        
+        rowTable = {}
+        colTable = {}
+        gridTable = {}
+        
+        for i in range(9):
+            rowTable[i] = set()
+            colTable[i] = set()
+            gridTable[i] = set()
+            
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if (board[row][col] != "."):
+                    val = board[row][col]
+                    if val in rowTable[row]: return False
+                    if val in colTable[col]: return False
+                    if val in gridTable[(row//3)*3+(col//3)]: return False
+                    
+                    rowTable[row].add(val)
+                    colTable[col].add(val)
+                    gridTable[(row//3)*3+(col//3)].add(val)
+                    
+        return True
