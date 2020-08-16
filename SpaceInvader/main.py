52c3bb41afe2809aa382edf25533427d28b965f1
JC <juancpani@gmail.com>
2020-08-16 18:40:43 -0400

    added key movement to main.py

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -1,27 +1,41 @@
 import pygame
+from pygame.locals import *
 
 WIDTH = 800
 HEIGHT = 600
 
 pygame.init()
-screen = pygame.set_mode((
-    WIDTH,
-    HEIGHT)
-)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-pygame.display.set_caption("Space Invader")
-icon = pygame.image.load(ufo.png)
+pygame.display.set_caption('Space Invader')
+icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-def player():
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 400
+playerX_delta = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 running = True
 while running:
     screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
-            running = False
+        if event.type == pygame.QUIT:
+            running = False  
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_delta = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_delta = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_delta = 0
     
-    player()
+    playerX += playerX_delta
+    player(playerX % WIDTH, playerY)
     pygame.display.update()
