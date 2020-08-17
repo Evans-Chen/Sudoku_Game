2ed653c828954db9df596da60b6c6538f84f3112
JC <juancpani@gmail.com>
2020-08-16 23:23:23 -0400

    Added momentum to space invaders

--- SpaceInvader/main.py
+++ SpaceInvader/main.py
@@ -1,41 +1,73 @@
-import pygame
+import pygame, sys
+
+clock = pygame.time.Clock()
+
 from pygame.locals import *
 
-WIDTH = 800
-HEIGHT = 600
-
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Display 
 pygame.display.set_caption('Space Invader')
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
-playerImg = pygame.image.load('player.png')
-playerX = 370
-playerY = 400
-playerX_delta = 0
+# Screen 
+WINDOW_SIZE = (500, 600)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
-def player(x, y):
-    screen.blit(playerImg, (x, y))
+# Player
+player_image = pygame.image.load('player.png')
+def player(location):
+    screen.blit(player_image, location)
+
+
+# enemyImg = pygame.image.load('enemy.png')
+# enemyX = 370
+# enemyY = 50
+# enemyX_delta = 0
+# def enemy(x, y):
+#     screen.blit(enemyImg, (x, y))
+
+moving_left = False
+moving_right = False
+player_location = [50, 50]
+player_y_momentum = 0
 
 running = True
 while running:
     screen.fill((0, 0, 0))
+    player(player_location)
+
+    # Momentum
+    if player_location[1] > WINDOW_SIZE[1]-player_image.get_height():
+        player_y_momentum = -player_y_momentum
+    else:
+        player_y_momentum += 0.2
+    player_location[1] += player_y_momentum
+
+    # Movement
+    if moving_left:
+        player_location[0] -= 4
+    if moving_right:
+        player_location[0] += 4
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False  
+        if event.type == QUIT:
+            running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_delta = -0.1
-            if event.key == pygame.K_RIGHT:
-                playerX_delta = 0.1
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_delta = 0
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_RIGHT:
+                moving_right = True
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                moving_left = False
+            if event.key == K_RIGHT:
+                moving_right = False
     
-    playerX += playerX_delta
-    player(playerX % WIDTH, playerY)
     pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
